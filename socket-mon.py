e4bcfb0fd1545b42e1eb3f8707e382d89a1f038a
akhi1312 <akhilesh.deowanshi@hotmail.com>
2017-02-16 11:14:33 -0800

    Update socket-mon.py

--- socket-mon.py
+++ socket-mon.py
@@ -1,3 +1,4 @@
+
 import psutil
 import collections
 
@@ -6,15 +7,18 @@ def main():
     pidlist =[]
     tcp_final =[]
     tcp_sockets = psutil.net_connections(kind='tcp')
-
+# Header
     print "PID,Local address,Remote address,Status"
+
+# All valid tcp connections 
     for a in tcp_sockets:
-        if a.pid not in [] and str(a.laddr) not in ["()","::"] and str(a.raddr)!= "()":
+        if a.pid != None and str(a.laddr) not in ["()", "::"] and str(a.raddr)!= "()":
             pidlist += [a.pid]
             tcp_final += [a]
     counts= collections.Counter(pidlist)
-    pidlist = sorted(pidlist ,key=counts.get ,reverse =True)
-    pidlist = set(pidlist)
+    pidlist = sorted(set(pidlist) ,key=counts.get ,reverse =True) 
+ 
+ # Printing Final Output 
     for e in pidlist:
         for c in tcp_final:
             if  c.pid == e:
@@ -24,3 +28,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
